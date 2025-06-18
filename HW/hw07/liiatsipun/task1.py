f3f9495a4aa3073fd3e9dbbd6f17d8e22d86b424
Liia <liya.pipiya@icloud.com>
2025-06-18 23:36:36 +0300

    hw07: implemented task1

---
+++ HW/hw07/liiatsipun/task1.py
@@ -0,0 +1,9 @@
+def get_bigger_number(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+    
+print(get_bigger_number(6, 17))
+print(get_bigger_number(1996, 2003))
+print(get_bigger_number(17, 14))
