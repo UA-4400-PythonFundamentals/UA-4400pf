fe194f803ba65b953bebfc6c8dc098b3159d09ac
Hryhorii Dovhaliuk <dgrigorij77@gmail.com>
2025-06-24 11:04:04 +0300

    Add hw08

---
+++ HW/hw08/salonaut/task01/main.py
@@ -0,0 +1,6 @@
+from utils import *
+from models import *
+
+
+
+print(list(filter(lambda st: not ("__" in st), dir())))
---
+++ HW/hw08/salonaut/task03/main.py
@@ -0,0 +1,26 @@
+from calculate import *
+
+def main():
+    print('1 - Calculate area of Rectangle \n2 - Calculate area of Triangle \n3 - Calculate area of Circle \n4 - Exit')
+    while True:
+        user_choice = int(input('Enter choice: '))
+        if user_choice == 1:
+            a = int(input('Enter width: '))
+            b = int(input('Enter height: '))
+            print(rectangle_area(a, b))
+        elif user_choice == 2:
+            a = int(input('Enter base: '))
+            h = int(input('Enter height: '))
+            print(triangle_area(a, h))
+        elif user_choice == 3:
+            r = int(input('Enter Radius: '))
+            print(circle_area(r))
+        elif user_choice == 4:
+            break
+        else:
+            print('Invalid input. ')
+
+
+
+if __name__ == "__main__":
+    main()
