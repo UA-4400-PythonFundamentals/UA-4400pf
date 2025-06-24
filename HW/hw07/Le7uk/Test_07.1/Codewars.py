6652167ab1b4f887b258295b33a9f2cd0efacee5
Oleksandr Hirnyi <hirnyilesyk@gmail.com>
2025-06-24 17:44:39 +0200

    Update Codewars

--- HW/hw07/Le7uk/Test_07.1/Codewars.py
+++ HW/hw07/Le7uk/Test_07.1/Codewars.py
@@ -32,7 +32,61 @@
 # print(reverse("Hello World"))  
 
 # Reverse List Order
-def reverse_list(l):
-    return list(reversed(l))
-# Example usage:
-print(reverse_list([1, 2, 3, 4, 5]))
+
+# def reverse_list(l):
+#     return list(reversed(l))
+# print(reverse_list([1, 2, 3, 4, 5]))
+
+# #Multiples of 3 or 5    
+# def solution(number):
+#     sum = 0
+#     if number < 0:
+#         return 0
+#     else:
+#         for i in range(number):
+#             if i % 3 == 0 or i % 5 == 0:
+#                 sum += i
+#         return sum
+
+# Will you make it?
+
+# def zero_fuel(distance_to_pump, mpg, fuel_left):
+#     if fuel_left*mpg >= distance_to_pump:
+#         return True
+#     else:
+#         return False
+   
+#Are You Playing Banjo?
+
+# def areYouPlayingBanjo(name):
+#     if name[0].lower() == 'r':
+#         return name + ' plays banjo'
+#     else:
+#         return name + ' does not play banjo'
+
+#Convert boolean values to strings 'Yes' or 'No
+
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return "Yes"
+#     else:
+#         return "No"
+
+#Counting sheep...
+
+# def count_sheeps(sheep):
+#     result = 0
+#     for i in sheep:
+#             if i == True:
+#                 result += 1
+#             else:
+#                 continue
+#     return result
+
+#Is this my tail?
+
+# def correct_tail(body, tail):
+#     if body[-1] == tail:
+#          return True
+#     else:
+#          return False
