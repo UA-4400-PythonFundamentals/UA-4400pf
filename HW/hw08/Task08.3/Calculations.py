3ff42ebaa5c8c4cb07c28ca8ffc08433c9bb0ccd
Oleksandr Hirnyi <hirnyilesyk@gmail.com>
2025-06-29 21:45:26 +0200

    Task 08.3

---
+++ HW/hw08/Task08.3/Calculations.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_area(radius):
+    return math.pi * pow(radius,2)
+
+def calculate_area_rectangle(length, width):
+    return length * width
+
+def calculate_area_triangle(base, height):
+    return 0.5 * base * height
