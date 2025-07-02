e8952341d09ba51f788dd29c4db9fe011f8cf21b
Andrii Surovtsev <pm.surovtsev.a@gmail.com>
2025-07-02 18:01:39 +0300

    HW08 added

---
+++ HW/hw08/andriisurovtsev/Task3.py
@@ -0,0 +1,19 @@
+from geometry import rectangle_area, triangle_area, circle_area
+def area_of():
+    area = int(input("Введить:\n1 - якщо це площа прямокутника\n2 - якщо це площа трикутника\n3 - якщо це площа кола\n"))
+    if area == 1:
+        length = float(input("Введіть довжину: "))
+        width = float(input("Введіть ширину: "))
+        area_of_rect = rectangle_area(length, width)
+        print("Площа прямокутника: ", area_of_rect)
+    elif area == 2:
+        base = float(input("Введіть основу: "))
+        height = float(input("Введіть висоту: "))
+        area_of_tria = triangle_area(base, height)
+        print("Площа трикутника: ", area_of_tria)
+    else:
+        radius = float(input("Введіть радіус: "))
+        area_of_circ = circle_area(radius)
+        print("Площа кола: ", area_of_circ)
+
+area_of()
