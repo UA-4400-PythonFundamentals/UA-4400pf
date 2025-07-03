6d98bd45b772895877834c91c4af2758cfd9a86b
Oleksandr Pechak <164561042+oleksandrpechak@users.noreply.github.com>
2025-07-03 19:27:56 +0300

    Added hw08 task2

--- HW/hw08/oleksandrpechak/task2/task02.py
+++ HW/hw08/oleksandrpechak/task2/task02.py
@@ -13,49 +13,30 @@ char = re.findall("[a-z]", pas)
 Char = re.findall("[A-Z]", pas)
 num = re.findall("\d", pas)
 specific = re.findall("[@,#,$]", pas)
-# specific1 = re.search(r'$',pas)
-# specific2 = re.search(r'#',pas)
-# specific3 = re.search(r'@', pas)
 
-# print(char)
-# print(Char)
-# print(num)
-# print(specific)
 result = []
 if True:
-    # print(True)
     if True:
         if len(pas) >= 6:
             result.append(True)
-            # print(True)
         if len(pas) <= 16:
             result.append(True)
-            # print(True)
         if len(specific) > 0 :
             result.append(True)
-            # print(True)
         for c in char:
             if len(c) >0:
                 result.append(True)
-                # print("char")
                 break
         else:
             result.append(False)
-            # print(False)
         if len(Char) > 0 :
-            # if len(x)>0:
-                result.append(True)
-                # print("Char")
+            result.append(True)
         else:
             result.append(False)
-            # print(False)
         if len(num) > 0:
-        # #     if len(i) > 0:
-                result.append(True)
-        # #         # print("Num")
+            result.append(True)
         else:
             result.append(False)
-        # #     # print(False)
 if sum(result) > 5 :
     print("The password was created")
 else:
