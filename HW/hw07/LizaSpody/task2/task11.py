008ba9612c3f59c47575e5dc272b550efd7c15de
Elizaveta Spodynska <elizavetasp@wizardsdev.com>
2025-07-08 00:16:28 +0200

    add HW7

---
+++ HW/hw07/LizaSpody/task2/task11.py
@@ -0,0 +1,6 @@
+def count_sheeps(sheep):
+    count = 0
+    for sh in sheep:
+        if sh:
+            count += 1
+    return count
---
+++ HW/hw07/LizaSpody/task2/task5.py
@@ -0,0 +1,4 @@
+def reverse(st):
+    arr = st.split()
+    arr.reverse()
+    return ' '.join(arr)
